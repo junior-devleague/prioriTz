1e9ec2c703ce39cea206aba9904a13dfedc815c3
RyanJaunzemis <khuang20@punahou.edu>
2016-07-11 13:52:19 -1000

    worked on list

--- js/listItems.js
+++ js/listItems.js
@@ -10,5 +10,20 @@ window.onload = function(){
 };
 
 function loadItems(data){
-	console.log(data);
+	console.log(data[0]);
+	for(var i = 0; i < data.length; i++){
+		var dateCreated = data[i].dateCreated;
+		var dateDue = data[i].dateDue;
+		var description = data[i].description;
+		var isHardGoal = data[i].isHardGoal;
+		var location = data[i].location;
+		var name = data[i].name;
+		var urgency = data[i].urgency; 
+
+		var container = document.createElement('div');
+		container.className = "itemContainer";
+		container.innerHTML = "Test";
+		document.getElementById('itemList').appendChild(container);
+	}
 }
+	
